965b9496c1cb8898df7dad8ef7743edb31bd565b
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2019-10-16 20:17:35 +0800

    done1

--- test.c
+++ test.c
@@ -2,28 +2,35 @@
 #include <stdlib.h>
 
 int main(){
-    int i, j, number;
+    int i, j, k, l, number;
     printf("Please enter a number: ");
     scanf("%d", &number);
     for (i = 1; i <= number; i++){
         if (i <= number){
-            printf("%*c", 2*(number-i)+1, '*');
+            printf("%*c", i, '*');
         }
         for (j = 1; j < i; j++){
-            // printf("%c", '*');
+            printf("%c", '*');
         }
-        puts("");
+        if (i <= number){
+            printf("%*c", 2*(number-i)+1, ' ');
+        }
+
+
+
+
+
+
+        printf("\n");
     }
-    // for (i = 1; i <= number; i++){
-    //     if (i <= number){
-    //         printf("%*c", 2*(number-i)+1, '*');
-    //     }
-    //     for (j = 1; j < i; j++){
-    //         // printf("%c", '*');
-    //     }
-    //     puts("");
-    // }
+    for (k = 1; k <= number; k++){
+        if (k <= number){
+            printf("%*c", 2*(number-k)+1, '*');
+        }
+        for (l = 1; l < k; l++){
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
     return 0;
 }
-
-
