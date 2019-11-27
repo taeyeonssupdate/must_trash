5ae6c9d48d496348a4de745971e8139d498dbc75
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2019-11-27 22:06:07 +0800

    191127

--- To:嚴智.c
+++ To:嚴智.c
@@ -6,17 +6,17 @@ int main(){
     printf("Please enter a number: "); //要求輸入
     scanf("%d", &number); //掃描輸入
     
-    for (i = 1; i <= number; i++){ //設定i=1 迴圈i+1 當i<=number停止迴圈 >> 三角形部分總共幾行
-        if (i == number){ //如果i=number代表已經進入最後一行且三小型已輸出完成
-            for (i = 1; i <= number; i++){ //開始製作下方矩形 >> 相同方法for迴圈製造需要輸出的5行
-                printf("%*c", number+1, '*'); //製作對齊線根據規律為number+1
-                for (j = 0; j < number+number+1; j++){ //相同方法並套用公式補齊後面字元 >> 左邊number個+右邊number個加上中間4個測試出來的範圍為number+number+2
-                    printf("%c", '*');
-                }
-                printf("\n"); //i迴圈換行
-            }
+    // for (i = 1; i <= number; i++){ //設定i=1 迴圈i+1 當i<=number停止迴圈 >> 三角形部分總共幾行
+    //     if (i == number){ //如果i=number代表已經進入最後一行且三小型已輸出完成
+    for (i = 1; i <= number; i++){ //開始製作下方矩形 >> 相同方法for迴圈製造需要輸出的5行
+        printf("%*c", number+1, '*'); //製作對齊線根據規律為number+1
+        for (j = 0; j < number+number+1; j++){ //相同方法並套用公式補齊後面字元 >> 左邊number個+右邊number個加上中間4個測試出來的範圍為number+number+2
+            printf("%c", '*');
         }
+        printf("\n"); //i迴圈換行
     }
+    //     }
+    // }
 
 
     for (i = number; i >= 1; i--){ //設定i=1 迴圈i+1 當i<=number停止迴圈 >> 三角形部分總共幾行
