15a394b2cb26ecfc541b9e13f31b0f72f5c1c052
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2019-12-04 15:04:34 +0800

    191204

--- forlooptowhileloop.c
+++ forlooptowhileloop.c
@@ -3,58 +3,63 @@
 #include <time.h>
 
 int main(void){
-    int i,j,player1,player2,all_count=0,win_count=0,player1_win_count=0,player2_win_count=0;
-    while (win_count == all_count){
-        printf("請問你要玩幾戰？\n");
-        scanf("%d",&all_count);
-        printf("請問你要玩幾勝？\n");
-        scanf("%d",&win_count);
-        if (win_count > all_count){
-            printf("Error: %d勝%d戰，你是在玩什麼XDD\n請重新輸入\n",win_count,all_count);
+    int i,p1,p2,ac=0,wc=0,p1w=0,p2w=0;
+    printf("==========================\n!!!猜拳遊戲開始!!!\n");
+    while (wc == 0 && ac == 0){
+        printf("請問你要玩幾戰？");
+        scanf("%d",&ac);
+        fflush(stdin);
+        printf("請問你要玩幾勝？");
+        scanf("%d",&wc);
+        fflush(stdin);
+        if (wc > ac){
+            printf("Error: %d勝%d戰，你是在玩什麼XDD\n請重新輸入\n",wc,ac);
         }
     }
-    for (i = 0; i < all_count; i++){
-        printf("第%d局\n",i+1);
-        player1 = 0; 
-        player2 = 0;
-        while ((player1 >= 0)&&(player1 <=3)){
+    for (i = 0; i < ac; i++){
+        printf("==========================\n第%d局\n", i + 1);
+        p1=0;
+        while (p1<1||p1>3){
             printf("玩家請輸入請輸入: ");
-            scanf("%d", &player1);
+            scanf("%d", &p1);
             fflush(stdin);
-            break;
         }
         srand(time(NULL));
-        player2 = rand()%3+1;
-        if (player1 == player2){
-            printf("玩家,電腦都出%d，和局。\n",player1);
-        } else if (player1 == 1 && player2 == 2){
+        p2 = rand()%3+1;
+        if (p1 == p2){
+            printf("玩家，電腦都出%d，和局。\n",p1);
+        } else if (p1 == 1 && p2 == 2){
             printf("玩家出剪刀，電腦出石頭，電腦獲勝。\n");
-            player2_win_count++;
-        } else if (player1 == 2 && player2 == 1){
+            p2w++;
+        } else if (p1 == 2 && p2 == 1){
             printf("玩家出石頭，電腦出剪刀，玩家獲勝。\n");
-            player1_win_count++;
-        } else if (player1 == 1 && player2 == 3){
+            p1w++;
+        } else if (p1 == 1 && p2 == 3){
             printf("玩家出剪刀，電腦出布，玩家獲勝。\n");
-            player1_win_count++;
-        } else if (player1 == 3 && player2 == 1){
+            p1w++;
+        } else if (p1 == 3 && p2 == 1){
             printf("玩家出布，電腦出剪刀，電腦獲勝。\n");
-            player2_win_count++;
-        } else if (player1 == 2 && player2 == 3){
+            p2w++;
+        } else if (p1 == 2 && p2 == 3){
             printf("玩家出石頭，電腦出布，電腦獲勝。\n");
-            player2_win_count++;
-        } else if (player1 == 3 && player2 == 2){
+            p2w++;
+        } else if (p1 == 3 && p2 == 2){
             printf("玩家出布，電腦出石頭，玩家獲勝。\n");
-            player1_win_count++;
+            p1w++;
+        }
+        printf("目前比數:%d:%d\n",p1w,p2w);
+        if (p1w<wc&&p2w<wc&&i==ac-1){
+            printf("和局再一局\n");
+            i--;
         }
     }
-    if ((player1_win_count==player2_win_count)||(player1_win_count<win_count && player2_win_count<win_count)){
-        printf("平手 比數%d:%d\n",player1_win_count,player2_win_count);
-    } else if (player1_win_count > player2_win_count){
-        printf("玩家贏了，比數%d:%d\n",player1_win_count,player2_win_count);
-    } else if (player1_win_count < player2_win_count){
-        printf("電腦贏了，比數%d:%D\n",player1_win_count,player2_win_count);
+    if ((p1w==p2w)||(p1w<wc && p2w<wc)){
+        printf("平手 比數%d:%d\n==========================", p1w, p2w);
+    } else if (p1w > p2w){
+        printf("玩家贏了，比數%d:%d\n==========================", p1w, p2w);
+    } else if (p1w < p2w){
+        printf("電腦贏了，比數%d:%D\n==========================", p1w, p2w);
     }
-    //和局要繼續比要用函數比較好用=.=
     return 0;
 }
 
