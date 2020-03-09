17f290c3423643a0f820cfd80268ddb356fb4058
taeyeonssupdate <mfpss96156@gmail.com>
2020-03-09 13:59:55 +0800

    3/9

--- c/c.c
+++ c/c.c
@@ -1,34 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(){
-    int i, j, x, y;
-    for (i=0;i<10;i++){
-        for (j=10;j>0;j--){
-            if (j>i){
-                printf("*");
-            }
-        }
-        printf("\n");
-    }
-    printf("=======================================================\n");
-    for (i = 0; i < 10; i++){
-        for (j = 10; j > 0; j--){
-            if (j > i){
-                printf(" ");
-            }
-        }
-        for (x = -1; x < 10; x++){
-            if (x < i){
-                printf("*");
-            }
-        }
-        for (y=0;y<=10;y++){
-            if (y<i){
-                printf("*");
-            }
-        }
-        printf("\n");
-    }
+int Addition(int,int), minus(int,int), multiply(int,int), divided(int,int);
+int main(void){
+    int a , b;
+    char c;
+    printf("Please input number: ");
+    scanf("%d", &a);
+    fflush(stdin);
+    printf("Please input math: ");
+    scanf("%c", &c);
+    fflush(stdin);
+    printf("Please input number: ");
+    scanf("%d", &b);
+    fflush(stdin);
+    switch (c){
+    case '+':
+        printf("%d+%d=%d\n", a, b, Addition(a, b));
+        break;
+    case '-':
+        printf("%d-%d=%d\n", a, b, minus(a,b));
+        break;
+    case '*':
+        printf("%d*%d=%d\n", a, b, multiply(a, b));
+        break;
+    case '/':
+        printf("%d/%d=%d\n", a, b, divided(a, b));
+        break;
+    case '%':
+        printf("%d%%xit%d=%d\n", a, b, divided2(a, b));
+        break;
+    };
     return 0;
+}
+int Addition(int num1, int num2){
+    int a;
+    a=num1+num2;
+    return a;
+}
+int minus(int num1, int num2){
+    int a;
+    a=num1-num2;
+    return a;
+}
+int multiply(int num1, int num2){
+    int a;
+    a=num1*num2;
+    return a;
+}
+int divided(int num1, int num2){
+    int a;
+    a=num1/num2;
+    return a;
+}
+int divided2(int num1, int num2){
+    int a;
+    a=num1&num2;
+    return a;
 }
