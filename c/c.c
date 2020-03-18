40e02a9c0d4d3c4c673fc515151d89b807ed313e
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2020-03-18 11:05:38 +0800

    fuck you

--- c/c.c
+++ c/c.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 int fib(int);
 
 int main(void){
@@ -18,62 +17,4 @@ int fib(int n){
     else{
         return (fib(n-1)+fib(n-2));
     }
-=======
-int Addition(int, int), minus(int, int), multiply(int, int), divided(int, int), divided2(int,int);
-int main(void){
-    int a , b;
-    char c;
-    printf("Please input number: ");
-    scanf("%d", &a);
-    fflush(stdin);
-    printf("Please input math: ");
-    scanf("%c", &c);
-    fflush(stdin);
-    printf("Please input number: ");
-    scanf("%d", &b);
-    fflush(stdin);
-    switch (c){
-    case '+':
-        printf("%d+%d=%d\n", a, b, Addition(a, b));
-        break;
-    case '-':
-        printf("%d-%d=%d\n", a, b, minus(a,b));
-        break;
-    case '*':
-        printf("%d*%d=%d\n", a, b, multiply(a, b));
-        break;
-    case '/':
-        printf("%d/%d=%d\n", a, b, divided(a, b));
-        break;
-    case '%':
-        printf("%d%%xit%d=%d\n", a, b, divided2(a, b));
-        break;
-    };
-    return 0;
-}
-int Addition(int num1, int num2){
-    int a;
-    a=num1+num2;
-    return a;
-}
-int minus(int num1, int num2){
-    int a;
-    a=num1-num2;
-    return a;
-}
-int multiply(int num1, int num2){
-    int a;
-    a=num1*num2;
-    return a;
-}
-int divided(int num1, int num2){
-    int a;
-    a=num1/num2;
-    return a;
-}
-int divided2(int num1, int num2){
-    int a;
-    a=num1&num2;
-    return a;
->>>>>>> 84ae343bff94d37d10dc96f305c71e9ccb031c13
 }
