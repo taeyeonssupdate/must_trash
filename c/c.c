353e4ca91fb185388efdce3d048ad5547e3f0871
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2020-03-28 00:34:50 +0800

    123

--- c/c.c
+++ c/c.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
-int fib(int);
+#define MAX 20
 
-int main(void){
-    int n ;
-    for (n=1;n<=10;n++){
-        printf("fib(%d)=%d\n",n,fib(n));
+int main(){
+    int i = 0, max = -1, min = -1, sum = 0, list[MAX], cache, y, found;
+    while (i!=MAX){
+        printf("第%d次輸入: ",i+1);
+        scanf("%d",&list[i]);
+        if (list[i] == -1){
+            printf("使用完畢\n");
+            break;
+        }
+        if (max == -1 || max < list[i]){
+            max = list[i];
+        }
+        if (min == -1 || min > list[i]){
+            min = list[i];
+        }
+        sum+=list[i];
+        printf("最大值=%d, 最小值=%d, 總和=%d, 平均數=%d\n", max, min, sum, sum/(i+1));
+        i++;
     }
-    return 0;
-}
-
-int fib(int n){
-    if(n==1||n==2){
-        return 1;
-    }
-    else{
-        return (fib(n-1)+fib(n-2));
+    i=0;
+    while (1){
+        printf("第%d次輸入: ", i+1);
+        scanf("%d", &cache);
+        if (cache == -1){
+            printf("結束查詢\n");
+            break;
+        }
+        y = 0, found=-1;
+        while (y!=MAX){
+            if (list[y] == cache){
+                found = list[y];
+                break;
+            }
+            y++;
+        }
+        if (found == -1){
+            printf("沒找到\n");
+        }
+        else{
+            printf("找到了，在第%d次輸入\n",y+1);
+        }
+        i++;
     }
 }
