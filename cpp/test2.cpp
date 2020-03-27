0a5f4a7568131abd871193e43b14e4ddf1cbab76
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2020-03-28 00:37:36 +0800

    123

--- cpp/test2.cpp
+++ cpp/test2.cpp
@@ -1,15 +1,29 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class cat_data{
+public:
+    int id;
+    string name;
+    double weight;
+};
+
+cat_data input(){
+    cat_data data;
+    cin >> data.id >> data.name >> data.weight;
+    return data;
+}
 
 int main(){
-    for (int i =0; i<10;++i){
-        if (i = 1){
-            printf("hellow");
-        } else if (i == 9){
-            printf("World");
-        } else if (i == 10){
-            printf("!");
-        }
+    cat_data data[3];
+    for (int i = 0; i < 3; i++)
+        data[i] = input();
+    for (int j = 0; j < 3; j++)
+    {
+        if (data[j].weight > 6.0)
+            cout << data[j].id << ' ' << data[j].name << ' ' << data[j].weight << ' ' << endl;
     }
-    printf("done");
     return 0;
 }
--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-x = int(input('Number: '))
-line = [str(x) for x in range(x*2+1)]
-print(line)
-for x in range(0,x):
-    print(line[x])
-for x in range(x,len(line)):
-    print(line[x])
+print('\n'.join([' '.join([input() for i in range(3)]) for i in range(3)]))
