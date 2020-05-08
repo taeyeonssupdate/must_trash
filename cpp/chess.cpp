778e24ec5cf6042d2f9479693fe1b880a16ed9d7
탱구 <29322913+taeyeonssupdate@users.noreply.github.com>
2020-05-09 00:11:46 +0800

    finish

--- cpp/chess.cpp
+++ cpp/chess.cpp
@@ -1,96 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void ChessBoard(char *);
+void UI(char *);
 int judge(char *);
-void ChessBoard(char *c){
+
+void UI(char *c){
    int i;
-   printf("當前ChessBoard\n");
-   for (i = 1; i < 10; i++){
-      if (c[i - 1] == '\0'){
-         printf("| (%d) |", i);
+   printf("當前局面\n");
+   for (i = 0; i < 9; i++){
+      if (c[i] == '\0'){
+         printf("%d   ", i+1);
       } else {
-         printf("|  %c  |", c[i - 1]);
+         printf("%c   ", c[i]);
       }
-      if (i % 3 == 0){
-         printf("\n-----------------------\n\n");
+      if (i%3 == 2){
+         printf("\n\n");
       }
    }
 }
 
 int judge(char *c){
-   if (c[0]==c[1] && c[1]==c[2]){
-      if (&c[0] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[3]==c[4] && c[4]==c[5]){
-      if (c[3] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[6]==c[7] && c[7]==c[8]){
-      if (c[6] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[0]==c[3] && c[3]==c[6]){
-      if (c[0] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[1]==c[4] && c[4]==c[7]){
-      if (c[1] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[2]==c[5] && c[5]==c[8]){
-      if (c[2] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[0]==c[4] && c[4]==c[8]){
-      if (c[0] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
-   } else if (c[2]==c[4] && c[4]==c[6]){
-      if (c[2] != '\0'){
-         return 1;
-      } else {
-         return 0;
-      }
+   if (c[0]==c[1] && c[1]==c[2] && c[1] != '\0'){
+      return 1;
+   } else if (c[3]==c[4] && c[4]==c[5] && c[4] != '\0'){
+      return 1;
+   } else if (c[6]==c[7] && c[7]==c[8] && c[7] != '\0'){
+      return 1;
+   } else if (c[0]==c[3] && c[3]==c[6] && c[3] != '\0'){
+      return 1;
+   } else if (c[1]==c[4] && c[4]==c[7] && c[4] != '\0'){
+      return 1;
+   } else if (c[2]==c[5] && c[5]==c[8] && c[5] != '\0'){
+      return 1;
+   } else if (c[0]==c[4] && c[4]==c[8] && c[4] != '\0'){
+      return 1;
+   } else if (c[2]==c[4] && c[4]==c[6] && c[4] != '\0'){
+      return 1;
+   } else {
+      return 0;
    }
 }
 
 int main(){
-   char c[9], sign[2] = {'X', 'O'};
-   int cache, i, player;
-   for (i = 0; i < 9; i++)
-      c[i] = '\0';
-   ChessBoard(c);
+   char c[9] = {'\0','\0','\0','\0','\0','\0','\0','\0','\0'}, sign[2] = {'O', 'X'};
+   int index, i=0; 
+   srand(time(NULL));
+   printf("電腦開始輸入0\n玩家開始輸入1:");
+   scanf("%d",&i);
+
+   if (i){
+      sign[0] = 'X';
+      sign[1] = 'O';
+   }
+
    for (i = 0; i < 9; i++){
-      player = i % 2 + 1;
-      printf("第%d個玩家輸入：", player);
-      scanf("%d", &cache);
-      if (c[cache-1] == '\0'){
-         c[cache - 1] = sign[i % 2];
-         ChessBoard(c);
-         if (judge(c)){
-            printf("玩家%d(%c)贏得比賽！\n",player,c[i-1]);
-            return 0;
+      printf("第%d局 ",i+1);
+      if (sign[i % 2] == 'X'){
+         printf("玩家輸入：");
+         scanf("%d", &index);
+      } else if (sign[i % 2] == 'O'){
+         index = (rand() % 8) + 1;
+      }
+      if (c[index-1] != '\0'){
+         if (sign[i%2] == 'X'){
+            printf("位置%d已被下注.\n",index);
          }
-      } else {
-         printf("Error: 位置%d已有人輸入.\n",cache);
          i--;
+         continue;
+      }
+      if (sign[i%2] == 'O'){
+         printf("電腦出：%d\n", index);
+      }
+      c[index - 1] = sign[i % 2];
+      UI(c);
+      if (judge(c) == 1){
+         printf("%c贏得比賽！\n", sign[i % 2]);
+         return 0;
       }
    }
    return 0;
