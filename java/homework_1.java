f72f4c577b6bdb68c85f569c2d186e4588571f78
taeyeonssupdate <mfpss96156@gmail.com>
2020-10-05 00:10:07 +0800

    homework_1

---
+++ java/homework_1.java
@@ -0,0 +1,16 @@
+/*
+    filename:homework_1
+    function:print some text
+
+    author:taeyeonssupdate
+    time:2020/10/04
+*/
+public class homework_1 {
+    public static void main(String[] args) {
+        System.out.println("資訊工程系");
+        System.out.println("B08170XXX");
+        System.out.println("XXX");
+        System.out.println("0800096000");
+        System.out.println("id@gmail.com");
+    }
+}
