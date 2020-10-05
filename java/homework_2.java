d83be9c765da1c2604ab4434b5cf1c4f9d2a602b
taeyeonssupdate <mfpss96156@gmail.com>
2020-10-05 12:17:00 +0800

    fix

--- java/homework_2.java
+++ java/homework_2.java
@@ -7,20 +7,22 @@
     time:2020/10/04
 */
 import java.util.Scanner;
+import java.lang.Math;
 
 public class homework_2 {
     public static void main(String[] args) {
         int x, y;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please input x: ");
+        System.out.printf("Please input x: ");
         x = scanner.nextInt();
-        System.out.print("Please input y: ");
+        System.out.printf("Please input y: ");
         y = scanner.nextInt();
+        System.out.printf("輸入二個為%d,%d\n", x, y);
         System.out.println("輸出結果為：");
         System.out.printf("%d+%d=%d\n", x, y, x + y);
         System.out.printf("%d-%d=%d\n", x, y, x - y);
         System.out.printf("%d*%d=%d\n", x, y, x * y);
-        System.out.printf("%d/%d=%f...%f\n", x, y, (float) x / (float) y, (float) x % (float) y);
+        System.out.printf("%d/%d=%d...%d\n", x, y, Math.round((float) x / (float) y),Math.round((float) x % (float) y));
     }
 }
