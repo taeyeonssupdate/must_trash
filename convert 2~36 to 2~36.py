8697f727e5373c353dbcd2dbef10abfb4b545922
taeyeonssupdate <mfpss96156@gmail.com>
2020-10-10 04:00:38 +0800

    convert 2~36 to 2~36

---
+++ convert 2~36 to 2~36.py
@@ -0,0 +1,18 @@
+import sys
+import math
+import argparse
+
+
+if __name__ == "__main__":
+    text = sys.argv[1]
+    op1 = int(sys.argv[2])
+    op2 = int(sys.argv[3])
+    letter_to_number = [ord(letter)-87 if type(letter) == str else int(letter) for letter in text]
+    op1_count = sum([word if not i else word*i*op1 for i, word in enumerate(letter_to_number[::-1])])
+    out = []
+    while op1_count >= op2:
+        out.append(str(op1_count % op2))
+        op1_count = math.floor(op1_count/op2)
+    out.append(str(op1_count))
+    out.reverse()
+    print(int(''.join(out)))
