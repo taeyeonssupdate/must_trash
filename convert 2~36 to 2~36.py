59ea91ce424b9c0bd063870113aabc4ab3d08639
taeyeonssupdate <mfpss96156@gmail.com>
2020-10-10 12:05:18 +0800

    add argparse

--- convert 2~36 to 2~36.py
+++ convert 2~36 to 2~36.py
@@ -1,18 +1,20 @@
-import sys
 import math
 import argparse
 
 
 if __name__ == "__main__":
-    text = sys.argv[1]
-    op1 = int(sys.argv[2])
-    op2 = int(sys.argv[3])
-    letter_to_number = [ord(letter)-87 if not letter.isdigit() else int(letter) for letter in text]
-    op1_count = sum([word if not i else word*(op1**i) for i, word in enumerate(letter_to_number[::-1])])
-    out = []
-    while op1_count >= op2:
-        out.append(str(op1_count % op2))
-        op1_count = math.floor(op1_count/op2)
-    out.append(str(op1_count))
-    out.reverse()
-    print(''.join([chr(int(letter)+87) if int(letter)>9 else letter for letter in out]).upper())
+    prase = argparse.ArgumentParser()
+    prase.add_argument('-a', required=True, type=str, help="輸入a")
+    prase.add_argument('-b', required=True, type=int, help="輸入b")
+    prase.add_argument('-c', required=True, type=int, help="輸入c")
+    args = prase.parse_args()
+
+    letter_to_int = [ord(letter)-87 if not letter.isdigit() else int(letter) for letter in args.a]
+    int_10 = sum([word if not i else word*(args.b**i) for i, word in enumerate(letter_to_int[::-1])])
+    out_number_system = []
+    while int_10 >= args.c:
+        out_number_system.append(str(int_10 % args.c))
+        int_10 = math.floor(int_10/args.c)
+    out_number_system.append(str(int_10))
+    out_number_system.reverse()
+    print(''.join([chr(int(letter)+87) if int(letter) > 9 else letter for letter in out_number_system]).upper())
