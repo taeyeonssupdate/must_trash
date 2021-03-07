5da059573c2f42970068756034de22e34a972f0e
taeyeonssupdate <mfpss96156@gmail.com>
2021-03-07 22:06:22 +0800

    build workplace

--- auto_clean_cache.py
+++ auto_clean_cache.py
@@ -1,10 +1,5 @@
-import os
-import sys
-import shutil
-base = '/Users/taeyeon/vscode/zerojudge/{}/debug'
-if os.path.exists(base.format('c')):
-    shutil.rmtree(base.format('c'))
-    os.mkdir(base.format('c'))
-if os.path.exists(base.format('cpp')):
-    shutil.rmtree(base.format('cpp'))
-    os.mkdir(base.format('cpp'))
+import os, shutil
+home = os.path.expanduser("~")
+if os.path.exists("{}/vscode/zerojudge/.vscode/c_debug_cache".format(home)):
+    shutil.rmtree("{}/vscode/zerojudge/.vscode/c_debug_cache".format(home))
+    os.mkdir("{}/vscode/zerojudge/.vscode/c_debug_cache".format(home))
