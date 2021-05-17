29195be2158dc4b9aa13a1490c3f326fe87e3314
taeyeonssupdate <mfpss96156@gmail.com>
2021-05-17 09:41:27 +0800

    fix

--- school/吃大便/answer6.c
+++ school/吃大便/answer6.c
@@ -5,9 +5,9 @@ int main(void)
 {
 
     char *dp = malloc(sizeof(char) * 26);
-    int sum = 0;
+    int i, sum = 0;
 
-    for (int i = 0; i < 26; i++)
+    for (i = 0; i < 26; i++)
     {
         sum += 65 + i;
         *(dp + i) = (char)65 + i;
