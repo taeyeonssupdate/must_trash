2fe28bdbfcb1292eaaca712becd7fde5608e6572
taeyeonssupdate <mfpss96156@gmail.com>
2021-05-17 08:44:23 +0800

    靠杯

---
+++ school/吃大便/answer2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct student
+{
+    char name[20];
+    int ID;
+    int age;
+} stud;
+
+void output(stud out);
+int main(int argc, char const *argv[])
+{
+    stud s1;
+    printf("Name: ");
+    scanf("%s", &s1.name);
+    printf("ID: ");
+    scanf("%d", &s1.ID);
+    printf("Age: ");
+    scanf("%d", &s1.age);
+    output(s1);
+    return 0;
+}
+
+void output(stud out)
+{
+    printf("%s\n", out.name);
+    printf("%d\n", out.ID);
+    printf("%d\n", out.age);
+}
