4a3e9af82790fa4312bd1122406d3d5bd21e8973
taeyeonssupdate <mfpss96156@gmail.com>
2021-05-17 08:54:36 +0800

    fix

--- school/吃大便/answer1.c
+++ school/吃大便/answer1.c
@@ -1,9 +1,16 @@
+#include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
     int array[10], *ptr;
     ptr = array;
-    for (int i = 0; i <10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         ptr[i] = i;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", ptr[i]);
     }
     return 0;
 }
