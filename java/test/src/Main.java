6aa924db41c2f21e9c8eabfc059962a3de02ed7c
taeyeonssupdate <mfpss96156@gmail.com>
2021-06-19 20:11:42 +0800

    add test java

---
+++ java/test/src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ImageIcon icon = new ImageIcon("java/test/test.jpg");
+
+        JLabel label = new JLabel();
+        label.setText("It's working???");
+        label.setIcon(icon);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.green)
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.add(label);
+        frame.setVisible(true);
+    }
+}
