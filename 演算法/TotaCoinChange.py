96973808b8d2f87d36f8eeae5bb1ae142d075d29
taeyeonssupdate <mfpss96156@gmail.com>
2022-01-04 10:05:42 +0800

    fix homework

--- 演算法/TotaCoinChange.py
+++ 演算法/TotaCoinChange.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def totalcoinchange(self, coins, amount):
+    def DP_totalcoinchange(self, coins, amount):
         rs = [amount+1] * (amount+1)
         rs[0] = 0
         for i in range(1, amount+1):
@@ -11,7 +11,38 @@ class Solution(object):
             return -1
         return rs[amount]
 
+    def BFS_totalcoinchange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        if amount == 0:
+            return 0
+        value1 = [0]
+        value2 = []
+        nc = 0
+        visited = [False]*(amount+1)
+        visited[0] = True
+        while value1:
+            nc += 1
+            for v in value1:
+                for coin in coins:
+                    newval = v + coin
+                    if newval == amount:
+                        return nc
+                    elif newval > amount:
+                        continue
+                    elif not visited[newval]:
+                        visited[newval] = True
+                        value2.append(newval)
+            value1, value2 = value2, []
+        return -1
 
-print(Solution().totalcoinchange([4, 1, 5, 10, 50], 129))
-print(Solution().totalcoinchange([4, 1, 5, 10, 50], 63))
-print(Solution().totalcoinchange([4, 1, 5, 10, 50], 37))
+
+print("DP 129:", Solution().DP_totalcoinchange([4, 1, 5, 10, 50], 129))
+print("DP 63:", Solution().DP_totalcoinchange([4, 1, 5, 10, 50], 63))
+print("DP 37:", Solution().DP_totalcoinchange([4, 1, 5, 10, 50], 37))
+print("BFS 129:", Solution().BFS_totalcoinchange([4, 1, 5, 10, 50], 129))
+print("BFS 63:", Solution().BFS_totalcoinchange([4, 1, 5, 10, 50], 63))
+print("BFS 37:", Solution().BFS_totalcoinchange([4, 1, 5, 10, 50], 37))
