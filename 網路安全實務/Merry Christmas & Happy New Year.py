5c040d4a34c3d7fecb5d4d62a56a2fe545442291
taeyeonssupdate <mfpss96156@gmail.com>
2022-01-13 01:32:08 +0800

    init project

--- 網路安全實務/Merry Christmas & Happy New Year.py
+++
@@ -1,4 +0,0 @@
-# Merry Christmas & Happy New Year, 8
-string, n = input(), int(input())
-out = [string[n*i:n*(i+1)] for i in range(len(string)//n)]
-[print(out[j][i], end='') for i in range(n) for j in range(len(out))]
--- 網路安全實務/歐幾里得.py
+++
@@ -1,8 +0,0 @@
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-    return a if b == 0 else gcd(b, a % b)
-
-
-if __name__ == "__main__":
-    print(gcd(552, 234))
