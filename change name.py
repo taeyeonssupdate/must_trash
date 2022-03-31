c8388ca8ba54e46975093763c5de5776a943f9f6
taeyeonssupdate <mfpss96156@gmail.com>
2022-03-31 17:44:55 +0800

    delete

--- change name.py
+++
@@ -1,7 +0,0 @@
-import os
-folders = ['A', 'B', 'C']
-
-for folder in folders:
-    for index, filename in enumerate(os.listdir(folder)):
-        os.renames(folder+'/'+filename, folder+'/'+str(index+1)+".jpg")
-        # time.sleep(0.1)
